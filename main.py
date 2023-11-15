fee24dd3465bb603839592c3f955e3c3bf4a7459
den192192 <danik.pdv@gmail.com>
2023-11-16 02:36:35 +0300

    aaaaaa

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.filters.command import Command
 from pymongo import MongoClient
-from datetime import date
+from datetime import datetime
+from handlers import get_router
 
 mongo = MongoClient()
 db = mongo.InformNovemberQuestBot
@@ -34,8 +35,8 @@ async def cmd_start(message: types.Message,state:FSMContext):
 
 @dp.message(F.text,startstates.beginstart)
 async def fio_status(message: types.Message,state:FSMContext):
-    today = date.today()
-    await state.update_data(UserId=message.from_user.id,Username=message.from_user.username,RegistrationDate=today.strftime("%d:%m:%Y"),FIO=message.text)
+    today = datetime.now()
+    await state.update_data(UserId=message.from_user.id,Username=message.from_user.username,RegistrationDate=today.strftime("%d:%m:%Y/%X"),FIO=message.text)
     await state.set_state(startstates.groupstate)
     await message.answer("Введите номер группы")
 
@@ -43,8 +44,15 @@ async def fio_status(message: types.Message,state:FSMContext):
 async def group_status(message:types.Message,state:FSMContext):
     data = await state.get_data()
     user_id_collection.insert_one({"UserId":data['UserId'], "username":data['Username'],"registrationDate":data['RegistrationDate'],"FIO":data['FIO'],"GroupNumber":message.text})
-    await message.answer("Регистрация пройдена. ")
+    await message.answer("Регистрация пройдена.")
+
+@dp.message(F.text.len()!=6,startstates.groupstate)
+async def group_status_mistake(message:types.Message):
+    await message.answer("Ошибка в номере группы, введите еще раз")
+
 async def main():
+    talk_router = get_router()
+    dp.include_router(talk_router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
